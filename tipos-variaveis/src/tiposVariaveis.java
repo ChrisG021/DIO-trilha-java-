bf52a037ff7b59516dbe8f557699865a8924a7e3
chris <christopheguerra67@gmail.com>
2025-03-31 09:57:17 -0300

    feat: add Operadores

--- tipos-variaveis/src/tiposVariaveis.java
+++ tipos-variaveis/src/tiposVariaveis.java
@@ -1,4 +1,10 @@
 public class tiposVariaveis {
+    int cep = 20;
+    long distancia = 2093475109374L;//"L" no final para representar pro compiler que é long
+    float pi = 3.142f;//Mesma coisa pro float
+    double area = 3.4;//mas n para o double
+    int numeronormal = 1;
+    short numerocruto = (short) numeronormal;//Cast de maior para menor
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
