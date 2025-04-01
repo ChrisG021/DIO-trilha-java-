ed3e875b6231df045ed9357fe191dc6d81104b4e
chris <christopheguerra67@gmail.com>
2025-04-01 18:29:36 -0300

    colocando exemplo for

--- java-controle-fluxo/src/App.java
+++ java-controle-fluxo/src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String nums[]={
+            "Carlos",
+            "Marcos",
+            "João"
+        };
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println( (i+1)+": "+nums[i]);
+        }   
     }
 }
